0221a963b8d2e981d1b7f848a6d2dd0a9d61c1ba
AcidTree <5029279+AcidTree@users.noreply.github.com>
2024-07-27 09:53:16 +1000

    update build

---
+++ src/test/java/com/datbear/GuardianInfoTest.java
@@ -0,0 +1,13 @@
+package com.datbear;
+
+import java.util.HashMap;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class GuardianInfoTest {
+  @Test
+  public void guardianInfoTest(){
+    //todo
+  }
+}
